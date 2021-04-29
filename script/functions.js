b910bc65aaac9a070868ef2af37fc6f663e8df31
Viktor <viktoreeeee@gmail.com>
2021-04-29 16:30:09 +0200

    brzo rijesenje

--- script/functions.js
+++ script/functions.js
@@ -35,16 +35,32 @@ function changeLightState() {
     Lights.changeState();
    // console.log(Lights.getCurrentStatus());
 
-    $.ajax({
-        url:"https://jsonstorage.net/api/items/e144ed31-7a6c-4a1b-8248-cf58a5c342be",
-        type:"PUT",
-        data:`{"light1":"${Lights.getCurrentStatus()}"}`,
-        contentType:"application/json; charset=utf-8",
-        dataType:"json",
-        success: function(data, textStatus, jqXHR){
-            console.log(data);
-        }
-    }); 
+   var url = "https://jsonstorage.net/api/items/e144ed31-7a6c-4a1b-8248-cf58a5c342be";
+
+   var json = JSON.stringify(`{"light1":"${Lights.getCurrentStatus()}"}`);
+   
+   var xhr = new XMLHttpRequest();
+   xhr.open("PUT", url, true);
+   xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+   xhr.onload = function () {
+       var users = JSON.parse(xhr.responseText);
+       if (xhr.readyState == 4 && xhr.status == "200") {
+        console.log(users);
+    } 
+   }
+  xhr.send(json);
+   
+
+    // $.ajax({
+    //     url:"https://jsonstorage.net/api/items/e144ed31-7a6c-4a1b-8248-cf58a5c342be",
+    //     type:"PUT",
+    //     data: `{"light1":"${Lights.getCurrentStatus()}"}`,
+    //     contentType:"application/json; charset=utf-8",
+    //     dataType:"json",
+    //     success: function(data, textStatus, jqXHR){
+    //         console.log(data);
+    //     }
+    // }); 
 }
 
 
